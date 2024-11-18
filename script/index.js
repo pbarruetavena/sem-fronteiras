8f3709b92d5a4af6f1c23fabd2816d223debbbf0
pbarruetavena <pbarruetavenavieira@gmail.com>
2024-11-18 20:28:11 -0300

    menu

--- script/index.js
+++ script/index.js
@@ -1,1 +1,24 @@
 //teste
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuItems = document.querySelectorAll(".menu-item");
+    const containers = document.querySelectorAll(".container");
+  
+    menuItems.forEach(item => {
+      item.addEventListener("click", () => {
+        menuItems.forEach(i => i.classList.remove("selected"));
+  
+        item.classList.add("selected");
+  
+        containers.forEach(container => {
+          container.classList.remove("visible");
+        });
+  
+        const targetId = item.getAttribute("data-target");
+        const targetContainer = document.getElementById(targetId);
+        if (targetContainer) {
+          targetContainer.classList.add("visible");
+        }
+      });
+    });
+  });
