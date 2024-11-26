0cabfd84fccce32ea315d6034888ee3bdd54ada0
Eduardo Costa de Souza <eduardbh2014@gmail.com>
2024-11-25 21:41:03 -0300

    Textos e tradução

--- script/index.js
+++ script/index.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 
-// seletor de linguagem
+// Seletor de linguagem
 const customSelect = document.querySelector('.custom-select');
 const selectedOption = document.querySelector('.selected-option');
 const optionsContainer = document.querySelector('.options');
@@ -39,7 +39,7 @@ const selectedFlag = document.getElementById('selected-flag');
 const selectedText = document.getElementById('selected-text');
 
 selectedOption.addEventListener('click', () => {
-    optionsContainer.style.display = 
+    optionsContainer.style.display =
         optionsContainer.style.display === 'block' ? 'none' : 'block';
 });
 
@@ -47,11 +47,14 @@ document.querySelectorAll('.option').forEach(option => {
     option.addEventListener('click', () => {
         const flagURL = option.getAttribute('data-img');
         const languageText = option.querySelector('span').textContent;
-
+        const langCode = option.getAttribute('data-value');
+        console.log(`translating to ${langCode}`);
         selectedFlag.src = flagURL;
         selectedText.textContent = languageText;
 
         optionsContainer.style.display = 'none';
+
+        translatePage(langCode);
     });
 });
 
@@ -60,3 +63,44 @@ document.addEventListener('click', (e) => {
         optionsContainer.style.display = 'none';
     }
 });
+
+// Initialize Google Translate Element
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'pt',
+        includedLanguages: 'en,pt,fr,ar',
+        autoDisplay: true,
+    }, 'google_translate_element');
+}
+
+function translatePage(langCode, iterations = 1) {
+    const googleSelect = document.querySelector("#google_translate_element select");
+    googleSelect.value = langCode;
+    googleSelect.dispatchEvent(new Event('change'));
+    if (googleSelect.value !== langCode && iterations <= 10) translatePage(langCode);
+}
+
+function waitForGoogleTranslate() {
+    const googleSelect = document.querySelector("#google_translate_element select");
+
+    if (googleSelect.value) updateCustomSelect(googleSelect);
+    else setTimeout(waitForGoogleTranslate, 100);
+}
+
+// Start polling when the window is loaded
+window.onload = () => {
+    waitForGoogleTranslate();
+};
+
+function updateCustomSelect(googleSelect) {
+    const currentLang = googleSelect.value;
+    const option = document.querySelector(`.option[data-value="${currentLang}"]`);
+
+    if (option) {
+        const flagURL = option.getAttribute('data-img');
+        const languageText = option.querySelector('span').textContent;
+
+        selectedFlag.src = flagURL;
+        selectedText.textContent = languageText;
+    }
+}
